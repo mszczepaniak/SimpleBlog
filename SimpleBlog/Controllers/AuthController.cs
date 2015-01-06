8aadd77779071ba78d1acb1bec20e1b40b54805e
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-06 13:53:45 +0100

    Added basic routes and controllers and the admin area.

---
+++ SimpleBlog/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Login()
+        {
+            return Content("Login!");
+        }
+    }
+}
