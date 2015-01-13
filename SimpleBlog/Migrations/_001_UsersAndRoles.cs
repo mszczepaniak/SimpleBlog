e0fc954b3cc46b785aeb50180198f7e1985cddec
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-13 23:39:36 +0100

    Establish SQL Database, use Fluent migrator via nuget package

---
+++ SimpleBlog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsCustom("VARCHAR(256)")
+                .WithColumn("password_hash").AsString(128);
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("users");
+        }
+    }
+}
