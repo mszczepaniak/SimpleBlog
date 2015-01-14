84d0c0608d38802d8ca9cb81adf2ecf0205b453e
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-14 20:50:47 +0100

    Finish the migration class, add the script to automatically execute the migration to mySql database

--- SimpleBlog/Migrations/_001_UsersAndRoles.cs
+++ SimpleBlog/Migrations/_001_UsersAndRoles.cs
@@ -1,4 +1,5 @@
-﻿using FluentMigrator;
+﻿using System.Data;
+using FluentMigrator;
 
 namespace SimpleBlog.Migrations
 {
@@ -12,12 +13,20 @@ namespace SimpleBlog.Migrations
                 .WithColumn("username").AsString(128)
                 .WithColumn("email").AsCustom("VARCHAR(256)")
                 .WithColumn("password_hash").AsString(128);
+            Create.Table("roles")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("name").AsString(128);
+            Create.Table("role_users")
+                .WithColumn("user_id").AsInt32().ForeignKey("users","id").OnDelete(Rule.Cascade)
+                .WithColumn("role_id").AsInt32().ForeignKey("roles","id").OnDelete(Rule.Cascade);
 
         }
 
         public override void Down()
         {
+            Delete.Table("role_users");
+            Delete.Table("roles");
             Delete.Table("users");
         }
     }
-}
+} 
---
+++ SimpleBlog/Tools/DeployDb-Dev.bat
@@ -0,0 +1,1 @@
+..\..\packages\FluentMigrator.1.4.0.0\tools\Migrate.exe --db=mysql --target=..\bin\SimpleBlog.dll --configPath=..\Web.config -c=MainDb
