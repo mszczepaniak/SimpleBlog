5fd3d7264a1e8b86eca791bd53fa3791a792f716
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-15 23:24:08 +0100

    Add Nhibernate via nuget, start to write data models

---
+++ SimpleBlog/Database.cs
@@ -0,0 +1,31 @@
+﻿using NHibernate.Cfg;
+
+namespace SimpleBlog
+{
+    public static class Database
+    {
+        public static void Configure()
+        {
+            var config = new Configuration();
+
+            //configure the connection string
+            // we do not provide any arguments here
+            // by default nhibernate will look to the web.config and get it from there
+            config.Configure(); 
+            //add our mappings
+             
+            //create session factory
+        }
+
+        public static void OpenSession()
+        {
+            
+        }
+
+        public static void CloseSession()
+        {
+            
+        }
+
+    }
+}
