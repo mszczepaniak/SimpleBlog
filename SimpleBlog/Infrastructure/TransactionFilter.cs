e41358cb362e77a510053a1dead95898af6c593f
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-18 20:38:57 +0100

    Finish and add Transaction Filter

---
+++ SimpleBlog/Infrastructure/TransactionFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace SimpleBlog.Infrastructure
+{
+    public class TransactionFilter : IActionFilter
+    {
+        // we create the filter
+        // happens when the action begins
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        { 
+            Database.Session.BeginTransaction();
+        }
+        // happens when the action is finished
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            // if there was no exception thrown by the action
+            if (filterContext.Exception == null)
+            {
+                Database.Session.Transaction.Commit();
+            }
+            //otherwise rollback the changes 
+            else
+            {
+                Database.Session.Transaction.Rollback();
+            }
+        }
+    }
+}
