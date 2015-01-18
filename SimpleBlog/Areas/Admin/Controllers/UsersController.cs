289bcc9d4fdda6633da05d3fe5390199a09c8c7b
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-18 01:50:20 +0100

    Add user modification in database via nhibernate

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -26,5 +26,26 @@ namespace SimpleBlog.Areas.Admin.Controllers
         {
             return View(new UsersNew {});
         }
+        [HttpPost]
+        public ActionResult New(UsersNew form)
+        {
+            if (Database.Session.Query<User>().Any(u => u.Username == form.Username))
+            {
+                ModelState.AddModelError("Username","Username must be unique");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+            var user = new User
+            {
+                Email = form.Email,
+                Username = form.Username,
+            };
+            user.SetPassword(form.Password);
+
+            Database.Session.Save(user);
+            return RedirectToAction("index");
+        }
     }
 }
